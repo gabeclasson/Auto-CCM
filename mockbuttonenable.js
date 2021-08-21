fa54860e5f76aefca3d295a4dc9573284130b59a
gabeclasson <54076045+gabeclasson@users.noreply.github.com>
2021-08-20 21:03:49 -0700

    Typo

--- mockbuttonenable.js
+++ mockbuttonenable.js
@@ -1,6 +1,6 @@
 /*
 Author: Gabe Classon
-This script is injected into the CAS-ILE tab; it enables the "mock close button" (a mock button that appear over the close button when a user has unsaved changes; if the user clicks the mock button, it asks them to confirm their selection to prevent lost work).
+This script is injected into the CAS-ILE tab; it enables the "mock close button" (a mock button that appears over the close button when a user has unsaved changes; if the user clicks the mock button, it asks them to confirm their selection to prevent lost work).
 */
 console.log("mockbuttonenable.js injected");
 var allStudentPanels = document.querySelectorAll(("[id^=coursepanel][id$=body]"));
